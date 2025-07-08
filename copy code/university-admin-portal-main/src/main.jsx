7397fc18b448a77ade75a904f03fd3d45a654864
umaboss <umairroar3@gmail.com>
2025-07-08 13:10:52 +0600

    updated latest

--- copy code/university-admin-portal-main/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-const rootElement = document.getElementById("root");
-if (rootElement) {
-  createRoot(rootElement).render(<App />);
-}
